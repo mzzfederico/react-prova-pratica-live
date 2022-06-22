e2d3d272ec5abfbac4b2d2ea9539170b1acc9fc3
Federico Muzzo <16427849+mzzfederico@users.noreply.github.com>
2022-06-22 17:57:03 +0200

    Aggiunto esercizio

--- src/DettaglioPokemon/index.jsx
+++ src/DettaglioPokemon/index.jsx
@@ -6,6 +6,20 @@ import PokemonSprites from "./Sprites";
 import PokemonGames from "./Games";
 
 export default function DettaglioPokemon() {
+
+    /*
+        Creiamo la pagina con la lista dei pokemon
+        la pagina tiene un contatore nello stato per la pagina
+        la pagina parte da 1 e aumenta...
+        passiamo alla richiesta 1 moltiplicato per il numero di pokemon (offset)
+        stampiamo i pokemon in pagina con l'immagine:
+        https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/<ID DEL POKEMON></ID>.png
+        i componenti dei pokemon, ognuno linkano alla pagina di dettaglio
+        ... aggiungiamo link alla pagina con la lista
+
+        BONUS: se salvate il numero di pagina nell'URL, usando useParams, rotta in Routes con :pagina...
+    */
+
     return (
         <PokemonProvider>
             <div className="pokemon">
